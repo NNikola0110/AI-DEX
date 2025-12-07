f69d7ecc1709ade07e7835ddab2267aebe63c001
Nikola Nikolić <nikolanikolic0110@gmail.com>
2025-12-07 23:42:01 +0100

    raydium-api filter fix

--- backend/src/raydium/raydium.controller.ts
+++ backend/src/raydium/raydium.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe, Query } from '@nestjs/common';
 import { RaydiumService } from './raydium.service';
 
 
@@ -11,11 +11,26 @@ constructor(private readonly raydiumService: RaydiumService) {}
     @Query('page') page = 1,
     @Query('limit') limit = 50,
   ) {
+    console.log(` NestJS route called: /raydium/pools?page=${page}&limit=${limit}`);
     const all = await this.raydiumService.getAllPools();
     const start = (page - 1) * limit;
     const end = start + limit;
     return all.slice(start, end);
   }
+
+@Get('pool/filtered')
+    async getFilteredPools(
+      @Query('sortField') sortField?: string,
+      @Query('sortType') sortType?: 'asc' | 'desc',
+      @Query('size', ParseIntPipe) size?: number,
+    ) {
+        console.log(` AANestJS route called: /raydium/pools/filtered?sortField=${sortField}&sortType=${sortType}&size=${size}`);
+      return this.raydiumService.getFilteredPools({
+        sortField,
+        sortType,
+        size,
+      });
+    }
 
   @Get('pools/:id')
   async getPool(@Param('id') id: string) {
@@ -23,28 +38,8 @@ constructor(private readonly raydiumService: RaydiumService) {}
   }
   
 
-  @Get('pools/filtered')
-  async getFilteredPools(
-    @Query('poolType') poolType?: string,
-    @Query('mintFilter') mintFilter?: string,
-    @Query('hasReward') hasReward?: string,
-    @Query('sortField') sortField?: string,
-    @Query('sortType') sortType?: 'asc' | 'desc',
-    @Query('size') size?: number,
-    @Query('nextPageId') nextPageId?: string,
-    @Query('mint1') mint1?: string,
-    @Query('mint2') mint2?: string,
-  ) {
-    return this.raydiumService.getFilteredPools({
-      poolType,
-      mintFilter,
-      hasReward: hasReward === 'true',
-      sortField,
-      sortType,
-      size,
-      nextPageId,
-      mint1,
-      mint2,
-    });
-  }
+  
+
+    
+
 }
